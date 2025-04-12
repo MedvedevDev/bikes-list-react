f603f73b5fe0037cd4ecab47868b0e4b2a074452
MedvedevDev <a95.medvedev@gmail.com>
2025-04-12 11:02:54 +0200

    first commit

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+import BikesList from "./BikesList.jsx";
+
+export default function Menu() {
+    return (
+        <main className='menu'>
+            <h2>Our Bikes</h2>
+            <BikesList />
+        </main>
+    )
+}
